3b192e9bbe03878b326a885490e01e66a31bc8f0
J0onYEong <cjy2134@naver.com>
2024-06-27 20:20:22 +0900

    [Fix] Inset설정수정및 이해완료

--- ChattingUI/Presentation/Sources/View/MyTextView.swift
+++ ChattingUI/Presentation/Sources/View/MyTextView.swift
@@ -29,11 +29,25 @@ class MyTextView: UITextView {
         self.delegate = self
         self.autocapitalizationType = .none
         
+        
+        // top, bottom값이 0이면 입력창이 너무좁아진다.
+        
+        
+        // contentInset은 내부컨텐츠 전체를 의미한다.
+        
+        // textContainerInset은 텍스트 필드 내부의 텍스트 상자의 인셋을 의미한다.
+        self.textContainerInset = .init(top: 5, left: 5, bottom: 5, right: 5)
+        self.textContainer.lineFragmentPadding = 0
+        
+        // textContainer.lineFragmentPadding은 텍스트 상자의 왼쪽 여백을 의미한다.
+        // textContainer와 textContainerInset는 Scroll상태일때만 잘동작한다.
+        // 두개가 동시에 설정되어 있고 ScrollEnabled가 false라면 양립하지 못하고 서로 밀어내게되어 UI가 깨진다. 주의하자
+        
         placeHolderLabel.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(placeHolderLabel)
         
         NSLayoutConstraint.activate([
-            placeHolderLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            placeHolderLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5),
             placeHolderLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
         ])
     }
